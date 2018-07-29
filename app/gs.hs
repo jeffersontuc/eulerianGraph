7cf74b656c99ae6cd0752834f19cbe0c0600e791
Natan7 <natan.ribeiro@ccc.ufcg.edu.br>
2018-07-29 02:38:17 -0300

    eulerian test fixed

--- app/gs.hs
+++ app/gs.hs
@@ -1,23 +1,28 @@
 import Data.Maybe
 import Control.Monad
 
-type Edge = (Char, Char)
+type Vertixe = Char
+type Edge = (Vertixe, Vertixe)
 type Graph = [Edge]
-
 
 countEdges :: Graph -> Int
 countEdges [] = 0
-countEdges (x:[]) = 1
-countEdges (x:xs) = 1 + countEdges xs
+countEdges (x:xs) | ((fst x)==' ' || (snd x)==' ') = countEdges xs
+                  | otherwise = 1 + (countEdges xs)
 
 countVertices :: Graph -> Int
 countVertices [] = 0
-countVertices (x:[]) = 2
-countVertices (x:xs) = length( compress (tupleToList (x:xs)) )
+countVertices (x:xs) = length( compress (removeWhiteSpaces (tupleToList (x:xs))) )
 
+isConected :: Graph -> Bool
+isConected [] = False
+isConected xs = ((countEdges xs) >= ((countVertices xs)-1)) && ((countEdges xs)>0 && (countVertices xs)>0)
+
+removeWhiteSpaces :: String -> String
+removeWhiteSpaces = filter (/=' ')
 
 tupleToList :: [(a, a)] -> [a]
-tupleToList ([]) = [] 
+tupleToList [] = [] 
 tupleToList (x:[]) = (fst x):(snd x):[]
 tupleToList (x:xs) = (fst x):(snd x):(tupleToList xs)
 
@@ -42,32 +47,39 @@ removeAll e (x:xs) | e == x = removeAll e xs
 - Retorna uma lista de pares com os elementos e suas quantidades. Ex: encode [2,2,2,3,4,2,5,2,4,5] = [(2,5),(3,1),(4,2),(5,2)]
 -}
 encode xs = compress ([(a,b) | a <- xs, b <- [0..(length xs)], b==(length (filter (a==) xs))])
-encode' xs = encode (tupleToList xs)
+encode' xs = encode (removeWhiteSpaces (tupleToList xs))
 {-
 - LEMBRAR - Um grafo G CONEXO possui caminho euleriano se e somente se ele tem exatamente ZERO ou DOIS vértices de grau impar. 
 -}
 
+degreeList :: [(a, t)] -> [t]
 degreeList [] = []
 degreeList (x:xs) = (snd x):(degreeList xs)
 
-typeGraph xs | and (map even xs) = putStrLn ("It's Eulerian cycle graph")
-             | even (length(filter odd xs)) = putStrLn ("It's Semi-Eulerian graph")
-             | otherwise = putStrLn ("It's not Eulerian graph")
+typeGraph xs ys | ( isConected ys ) && ( and (map even xs) ) = putStrLn ("It's Eulerian cycle graph")
+                | ( isConected ys ) && ( even (length(filter odd xs)) ) = putStrLn ("It's Semi-Eulerian graph")
+                | otherwise = putStrLn ("It's not Eulerian graph")
 
+--exemplo teste-----------------------------------------------
+grafo1 = [('a', 'b'), ('b', 'c')] 
+grafo2 = [('a', ' '), ('b', ' ')] -- ' ' diz que não está conectado 
 -------------------------------------------------
-grafo = [('a', 'b'), ('b', 'c')] -- exemplo teste
--------------------------------------------------
+
 
 main :: IO ()
 main = do
+  putStrLn ""
   print "Inform the graph: (Ex: [('a', 'b'), ('b', 'c')])"
   input <- getLine
   let graph = read input
   let numberEdges = countEdges graph
   let numberVertices = countVertices graph
   let degreesVertices = encode' graph
-  let result = typeGraph (degreeList (encode' graph))
-  putStrLn ("This graph have " ++ (show(numberEdges)) ++ " edge/es.")
-  putStrLn ("This graph have " ++ (show(numberVertices)) ++ " vertixe/ces.")
-  putStrLn ("Each vertixe this graph, have the following degree/es: " ++ (show(degreesVertices)))
+  let result = typeGraph (degreeList (encode' graph)) graph
+  putStrLn ""
+  putStrLn ("Then...")
+  putStrLn ""
+  putStrLn ("This graph has " ++ (show(numberEdges)) ++ " edge/es.")
+  putStrLn ("This graph has " ++ (show(numberVertices)) ++ " vertixe/ces.")
+  putStrLn ("Each vertixe this graph, has the following degree/es: " ++ (show(degreesVertices)))
   result
