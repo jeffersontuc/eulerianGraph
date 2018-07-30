ee2caaba9337ba52916c8c47c0e2976c8197eb1f
Natan7 <natan.ribeiro@ccc.ufcg.edu.br>
2018-07-30 05:03:50 -0300

    fixed isconnected

--- app/gs.hs
+++ app/gs.hs
@@ -1,4 +1,5 @@
 import Data.Maybe
+import Data.List
 import Control.Monad
 
 type Vertex = Char
@@ -22,13 +23,25 @@ countVertices [] = 0
 countVertices (x:xs) = length( compress (removeWhiteSpaces (tuplesToString ( compress (x:xs) ))) )
 
 {-
--- Remove arestas para calcular as arestas. 
--- Ex: removeEdgesNotConected [('a', 'a'), (' ','b'),(' ',' ')] = []
+-- Ordena caracteres entre tuplas.
 -}
-removeEdgesNotConected :: Graph -> Graph
-removeEdgesNotConected [] = []
-removeEdgesNotConected (x:xs) | ((fst x)==' ' || ((fst x)==(snd x))) = removeEdgesNotConected xs
-                           | otherwise = x:(removeEdgesNotConected xs)
+sortTuple :: Ord a => (a,a) -> (a,a)
+sortTuple (x,y) | x>y = (y,x)
+                | otherwise = (x,y)
+{-
+-- Ordena caracteres entre tuplas de uma lista de tuplas.
+-}
+sortTuplesInList :: Ord a => [(a,a)] -> [(a,a)]
+sortTuplesInList xs = map sortTuple xs
+
+{-
+-- Remove arestas para calcular as arestas. 
+-- Ex: removeEdgesNotConnected [('a', 'a'), (' ','b'),(' ',' ')] = []
+-}
+removeEdgesNotConnected :: Graph -> Graph
+removeEdgesNotConnected [] = []
+removeEdgesNotConnected (x:xs) | ((fst x)==' ' || ((fst x)==(snd x))) = removeEdgesNotConnected xs
+                           | otherwise = x:(removeEdgesNotConnected xs)
 
 {-
 -- Remove espaço de Strings (ou lista de caracteres).
@@ -56,29 +69,17 @@ compress (x:xs) | (elem x xs) = compress (x:(remove x xs))
 
 {-
 -- Retorna uma lista de pares com os elementos e suas quantidades. 
--- Ex: encode' "asdaaaaaa" = [('a',7),('s',1),('d',1)]
+-- Ex: encode "asdaaaaaa" = [('a',7),('s',1),('d',1)]
 -}
-encode' :: Eq a => [a] -> [(a, Int)]
-encode' xs = compress ( [(a,b) | a <- xs, b <- [0..(length xs)], b==(length (filter (a==) xs))] )
-
-{-
--- Ordena caracteres entre tuplas.
--}
-sortTuple :: Ord a => (a,a) -> (a,a)
-sortTuple (x,y) | x>y = (y,x)
-                | otherwise = (x,y)
-{-
--- Ordena caracteres entre tuplas de uma lista de tuplas.
--}
-sortTuplesInList :: Ord a => [(a,a)] -> [(a,a)]
-sortTuplesInList xs = map sortTuple xs
+encode :: Eq a => [a] -> [(a, Int)]
+encode xs = compress ( [(a,b) | a <- xs, b <- [0..(length xs)], b==(length (filter (a==) xs))] )
 
 {-
 -- Calcula graus dos vertices não nulos, renornando o Vertice e seu respectivo grau. 
 -- Ex: calculatingDegrees [('a', 'b'), ('b', 'c')] = [('a',1),('b',2),('c',1)]
 -}
 calculatingDegrees :: Graph -> [(Vertex, Int)]
-calculatingDegrees xs = encode' (removeWhiteSpaces (tuplesToString ( removeEdgesNotConected (compress (sortTuplesInList xs)) )))
+calculatingDegrees xs = encode (removeWhiteSpaces (tuplesToString ( removeEdgesNotConnected (compress (sortTuplesInList xs)) )))
 
 {-
 -- Converte uma lista de tuplas com graus dos vertices, para uma lista de inteiros dos graus. 
@@ -101,8 +102,8 @@ tuplesToString (x:xs) = (fst x):(snd x):(tuplesToString xs)
 -- Função auxiliar de typeGraph, que verifica e imprime se o grafo não é euleriano, é euleriano ou semi-euleriano 
 -}
 typeGraph' :: Integral a => [a] -> Graph -> IO ()
-typeGraph' xs ys | ( isConected ys ) && ( and (map even xs) ) = putStrLn ("It's Eulerian cycle graph")
-                | ( isConected ys ) && ( even (length(filter odd xs)) ) = putStrLn ("It's Semi-Eulerian graph")
+typeGraph' xs ys | ( isConnected ys ) && ( and (map even xs) ) = putStrLn ("It's Eulerian cycle graph")
+                | ( isConnected ys ) && ( even (length(filter odd xs)) ) = putStrLn ("It's Semi-Eulerian graph")
                 | otherwise = putStrLn ("It's not Eulerian graph")
 
 typeGraph :: [(Vertex, Int)] -> Graph -> IO ()
@@ -126,12 +127,18 @@ degreeZeroVertices xs (y:ys) | elem y xs = degreeZeroVertices xs ys
                              | otherwise = ( (y,0) ):(degreeZeroVertices xs ys)
 
 {-
--- Retorna se o grafo é conectado ou não.
+-- Retorna se o grafo é connectado ou não.
 -}
-isConected :: Graph -> Bool
-isConected [] = False
-isConected xs = ((countEdges xs) >= ((countVertices xs)-1)) && ((countEdges xs)>0 && (countVertices xs)>0)
-
+{-
+--isConnected :: Graph -> Bool
+--isConnected [] = False
+--isConnected xs = ((countEdges xs) >= ((countVertices xs)-1)) && ((countEdges xs)>0 && (countVertices xs)>0)
+-}
+g a ([]) ([]) = True
+g a (x:xs) ([]) = True
+g a ([]) (y:ys) = False
+g a (x:xs) (y:ys) | (a/=(snd x)) = g (snd x) xs ys
+                  | otherwise = g (snd x) (xs) (ys)
 main :: IO ()
 main = do
   -- Recebendo o grafo
@@ -143,11 +150,15 @@ main = do
   -- Calculando dados sobre o grafo
   let numberEdges = countEdges graph
   let numberVertices = countVertices graph
-  let degreesVertices = calculatingDegrees graph
+
   let verticesList = (compress (removeWhiteSpaces (tuplesToString graph)))
+
+  let degreesVertices = calculatingDegrees graph
   let degreeZeroVerticesList = degreeZeroVertices (verticesWithDegree degreesVertices) verticesList
   let allDegreesVerticesList = degreesVertices ++ degreeZeroVerticesList
 
+  --let t = g (fst (head (sort (sortTuplesInList (compress graph)))) ) (sort (sortTuplesInList (compress graph))) verticesList
+  let t = g (fst (head ((sortTuplesInList ( graph)))) ) ((sortTuplesInList ( graph))) verticesList
   -- Imprimindo os resultados
   putStrLn ""
   putStrLn ("Then...")
@@ -155,5 +166,7 @@ main = do
   putStrLn ("This graph has " ++ (show numberEdges) ++ " edges.")
   putStrLn ("This graph has " ++ (show numberVertices) ++ " vertices.")
   putStrLn ("Each vertex this graph, has the following degrees: " ++ (show allDegreesVerticesList))
-  --typeGraph degreesVertices graph
   typeGraph allDegreesVerticesList graph
+
+  print ( (verticesList/="") && (degreeZeroVerticesList==[]) && t )
+  
